98e3f32f4eab453865ff11a8e1db4d640bff998a
Daniele Pandolfo <cocomelone46@gmail,com>
2021-11-25 16:47:38 +0100

    primo

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.but);
+    }
+    public void ww(View view){
+        Toast toast = Toast.makeText(this, "ciaoo",
+                //make test ==new
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+}
